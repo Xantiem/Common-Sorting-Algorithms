79820a8c7d935ee77902b8bac74a4513e009a619
Nicholas <35278540+Xantiem@users.noreply.github.com>
2025-07-25 16:40:54 +0200

    Add files via upload

--- InsertionSort.h
+++ InsertionSort.h
@@ -5,7 +5,7 @@ void insertion_sort(int *array, int arr_size) {
     for (int i=1; i<arr_size; ++i) {
         var = array[i];
         prev_var = i-1;
-        while((prev_var>=0) && (var < array[prev_var])) {
+        while((prev_var>=0) && (var < array[prev_var])) { // > decending < ascending
             array[prev_var +1] = array[prev_var];
             --prev_var;
         }
